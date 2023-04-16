215390f64e0f536460f73a8078ac64634248c4de
Akash Shaw <shawakash2003@gmail.com>
2023-04-16 12:27:50 +0530

    Learning To Add css and Tailwind css from External File

--- hunting_coder/src/pages/index.js
+++ hunting_coder/src/pages/index.js
@@ -3,7 +3,8 @@ import { Inter } from 'next/font/google'
 import Head from 'next/head'
 import Script from 'next/script'
 import Link from 'next/link'
-// import mainStyles from '../styles/taillwindCss'
+import { mainStyles } from '@/styles/taillwindCss'
+// import mainStyles from '../styles/taillwindCss.js'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,60 +20,71 @@ export default function Home() {
       <Script src='/api/hello.js' strategy='lazyOnload' onError={() => console.log('Hola')}>
         alert('Hola')
       </Script>
-      <nav className="flex justify-center items-center py-10">
-        <ul className="flex flex-row gap-x-10 text-xl font-semibold">
+      <nav className={`${mainStyles.mainnav}`}>
+        <ul className={`${mainStyles.navList}`}>
           <Link href='/' scroll={true} prefetch={true}>
-            <li className="">Home</li>
+            <li className={`${mainStyles.navItem}`}>Home</li>
           </Link>
           <Link href='/about' prefetch={true}>
-          <li className="">About</li>
+            <li className={`${mainStyles.navItem}`}>About</li>
           </Link>
           <Link href='/me'>
-            <li className="">Me</li>
+            <li className={`${mainStyles.navItem}`}>Me</li>
           </Link>
           <Link href='/blog'>
-            <li className="">Blogs</li>
+            <li className={`${mainStyles.navItem}`}>Blogs</li>
           </Link>
           <Link href='/bloggers'>
-            <li className="">Bloggers</li>
+            <li className={`${mainStyles.navItem}`}>Bloggers</li>
           </Link>
           <Link href='/contact'>
-            <li className="">Contact</li>
+            <li className={`${mainStyles.navItem}`}>Contact</li>
           </Link>
         </ul>
       </nav>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24 pt-10">
-        <div className='flex flex-col gap-y-16 justify-center items-center'>
-          <div className="flex flex-col leading-4 gap-y-5 text-center text-clip">
-            <h1 className="text-black font-bold  text-5xl ">Hunting Coder</h1>
-            <p className='tracking-wide text-xl'>A BlogPost For Hunting Coders, By Hunting Coders and Of Hunting Coders :)</p>
+      <main className={`${mainStyles.main}`}>
+
+        <div className={`${mainStyles.mainBodyContainer}`}>
+
+          <div className={`${mainStyles.heroContainer}`}>
+            <h1 className={`${mainStyles.heroHeading}`}>Hunting Coder</h1>
+            <p className={`${mainStyles.heroDescr}`}>A BlogPost For Hunting Coders, By Hunting Coders and Of Hunting Coders :)</p>
           </div>
-          <div className="blogPosts flex flex-col gap-y-7 items-center ">
-            <div className="blogHeading ">
-              <h2 className="font-bold text-2xl tracking-wide border-b-2 w-fit px-2 border-dashed border-b-black py-2">BlogPosts</h2>
-            </div>
-            <div className="blog tracking-wide leading-5 w-1/2 flex flex-col gap-y-2">
+
+          <div className={`${mainStyles.blogPostsContainer}`}>
+            
+            <Link href='/blog'>
+              <div className="blogHeading ">
+                <h2 className={`${mainStyles.blogTitle}`}>BlogPosts</h2>
+              </div>
+            </Link>
+
+            <div className={`${mainStyles.blogContainer}`}>
               <Link href='/blogpost/DosAndDont'>
-                  <h3 className="font-semibold text-2xl">Do's and Don't's before Switching between Frameworks</h3>
+                <h3 className={`${mainStyles.blogHeading}`}>Do's and Don't's before Switching between Frameworks</h3>
               </Link>
-              <p className="font-normal w-fit text-slate-600 text-lg">Lorem ipsum dolor sit  quisquam odit iure, ullam veniam enim saepe sequi dolores voluptatibus quidem assumenda minus expedita corrupti quae beatae voluptatum eius delectus sed quis. Voluptatem sint et eveniet placeat. Nulla nobis nisi ab ullam quisquam sequi? Possimus totam perferendis autem.</p>
+              <p className={`${mainStyles.blogDescr}`}>Lorem ipsum dolor sit  quisquam odit iure, ullam veniam enim saepe sequi dolores voluptatibus quidem assumenda minus expedita corrupti quae beatae voluptatum eius delectus sed quis. Voluptatem sint et eveniet placeat. Nulla nobis nisi ab ullam quisquam sequi? Possimus totam perferendis autem.</p>
             </div>
-            <div className="blog tracking-wide leading-5 w-1/2 flex flex-col gap-y-2">
+
+            <div className={`${mainStyles.blogContainer}`}>
               <Link href='/blogpost/DosAndDont'>
-                  <h3 className="font-semibold text-2xl">Do's and Don't's before Switching between Frameworks</h3>
+                <h3 className={`${mainStyles.blogHeading}`}>Do's and Don't's before Switching between Frameworks</h3>
               </Link>
-              <p className="font-normal w-fit text-slate-600 text-lg">Lorem ipsum dolor sit  quisquam odit iure, ullam veniam enim saepe sequi dolores voluptatibus quidem assumenda minus expedita corrupti quae beatae voluptatum eius delectus sed quis. Voluptatem sint et eveniet placeat. Nulla nobis nisi ab ullam quisquam sequi? Possimus totam perferendis autem.</p>
+              <p className={`${mainStyles.blogDescr}`}>Lorem ipsum dolor sit  quisquam odit iure, ullam veniam enim saepe sequi dolores voluptatibus quidem assumenda minus expedita corrupti quae beatae voluptatum eius delectus sed quis. Voluptatem sint et eveniet placeat. Nulla nobis nisi ab ullam quisquam sequi? Possimus totam perferendis autem.</p>
             </div>
-            <div className="blog tracking-wide leading-5 w-1/2 flex flex-col gap-y-2">
+
+            <div className={`${mainStyles.blogContainer}`}>
               <Link href='/blogpost/DosAndDont'>
-                  <h3 className="font-semibold text-2xl">Do's and Don't's before Switching between Frameworks</h3>
+                <h3 className={`${mainStyles.blogHeading}`}>Do's and Don't's before Switching between Frameworks</h3>
               </Link>
-              <p className="font-normal w-fit text-slate-600 text-lg">Lorem ipsum dolor sit  quisquam odit iure, ullam veniam enim saepe sequi dolores voluptatibus quidem assumenda minus expedita corrupti quae beatae voluptatum eius delectus sed quis. Voluptatem sint et eveniet placeat. Nulla nobis nisi ab ullam quisquam sequi? Possimus totam perferendis autem.</p>
+              <p className={`${mainStyles.blogDescr}`}>Lorem ipsum dolor sit  quisquam odit iure, ullam veniam enim saepe sequi dolores voluptatibus quidem assumenda minus expedita corrupti quae beatae voluptatum eius delectus sed quis. Voluptatem sint et eveniet placeat. Nulla nobis nisi ab ullam quisquam sequi? Possimus totam perferendis autem.</p>
             </div>
+
           </div>
         </div>
+
         <Link href='/' scroll={true}>
-          <button className="bottom-2 border-black border rounded-lg p-2">
+          <button className={`${mainStyles.primaryButton}`}>
             Page Up
           </button>
         </Link>
--- hunting_coder/src/styles/taillwindCss.js
+++ hunting_coder/src/styles/taillwindCss.js
@@ -1,3 +1,16 @@
 export const mainStyles = {
-    mainContainer: 'flex justify-center items-center',
+    mainnav: 'flex justify-center items-center py-10',
+    navList: 'flex flex-row gap-x-10 text-xl font-semibold',
+    navItem: 'cursor-pointer px-2 py-1',
+    main: 'flex min-h-screen flex-col items-center justify-between p-24 pt-10',
+    mainBodyContainer: 'flex flex-col gap-y-16 justify-center items-center',
+    heroContainer: 'flex flex-col leading-4 gap-y-5 text-center text-clip',
+    heroHeading: 'text-black font-bold  text-5xl ',
+    heroDescr: 'tracking-wide text-xl',
+    blogPostsContainer: 'blogPosts flex flex-col gap-y-7 items-center ',
+    blogContainer: 'blog tracking-wide leading-5 w-1/2 flex flex-col gap-y-2',
+    blogTitle: 'font-bold text-2xl tracking-wide border-b-2 w-fit px-2 border-dashed border-b-black py-2',
+    blogHeading: 'font-semibold text-2xl',
+    blogDescr: 'font-normal w-fit text-slate-600 text-lg',
+    primaryButton: 'bottom-2 border-black border rounded-lg p-2',
 }
