9adbce468fd0843f1ab3e35e2d73199f511fe0f0
Akash Shaw <shawakash2003@gmail.com>
2023-04-18 19:58:08 +0530

    Creating Blog Posts Json and Blog Page

--- hunting_coder/src/pages/blogpost/[slug].js
+++ hunting_coder/src/pages/blogpost/[slug].js
@@ -1,6 +1,10 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import { useRouter } from 'next/router'
 import React from 'react'
+
+// Find the correct post using slug
+// Fill the page
 
 const slug = () => {
     const router = useRouter();
@@ -14,9 +18,12 @@ const slug = () => {
                 <meta name="keywords" content="huting coder, hunting, coder, nextJs, nextjs, About Hunting Coders" />
             </Head>
 
-            <main className="">
-                <div className='font-bold text-3xl'>Accessed Sub-Pathname: {slug}</div>
-            </main>
+            <div className={`blog tracking-wide leading-5 w-1/2 flex flex-col gap-y-10`}>
+                <Link href='/blogpost/DosAndDont'>
+                    <h3 className={`font-semibold text-4xl`}>Do's and Don't's before Switching between Frameworks</h3>
+                </Link>
+                <p className={`font-semibold w-fit text-slate-600 text-xl`}>Lorem ipsum dolor sit  quisquam odit iure, ullam veniam enim saepe sequi dolores voluptatibus quidem assumenda minus expedita corrupti quae beatae voluptatum eius delectus sed quis. Voluptatem sint et eveniet placeat. Nulla nobis nisi ab ullam quisquam sequi? Possimus totam perferendis autem. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima asperiores architecto blanditiis, obcaecati dicta laboriosam corporis odio itaque perferendis nesciunt quia? At, sequi obcaecati porro atque nemo minima placeat? Harum, voluptate ducimus quidem tempora pariatur libero veniam beatae, fugiat totam quasi sed doloremque, asperiores similique explicabo quos est? Pariatur corrupti, eveniet illo totam vero minima eos! Aperiam itaque corporis fugiat repellendus. Exercitationem aperiam voluptatem ipsam qui numquam, corporis, dolorem aut cumque officiis non id est ipsa dolorum accusantium cum delectus? Velit facilis qui minus ratione, vitae non, reiciendis voluptas ea quis incidunt quasi officia nemo rem eum, cumque deserunt ullam!</p>
+            </div>
         </>
     )
 }
