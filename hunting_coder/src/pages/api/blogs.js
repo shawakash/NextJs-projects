e42bc11d116baa00310f921ff3820613efd9aa39
Akash Shaw <shawakash2003@gmail.com>
2023-04-18 21:32:43 +0530

    Fixing blog.js Api

--- hunting_coder/src/pages/api/blogs.js
+++ hunting_coder/src/pages/api/blogs.js
@@ -1,22 +1,33 @@
 import * as fs from 'fs';
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const { slug } = req.query;
-  const blogs = [];
-  fs.readdir('blogPost', 'utf8', (err, data) => {
-    if (err) {
-      return res.status(500).json({ err });
-    }
-    // data.forEach(file => {
-    //   fs.readFile(`blogPost/${file}`, 'utf-8', (fileErr, fileData) => {
-    //     if (fileErr) {
-    //       return res.status(500).json({err});
-    //     }
-    //     const file = JSON.parse(fileData)
-    //     blogs.push(file);
-    //     console.log(blogs)
-    //   });
-    // });
-    res.status(200).json({ data });
-  });
+  let blogs = [];
+  let data = await fs.promises.readdir('blogPost')
+  for(let file of data) {
+    console.log(file)
+    let fileData = await fs.promises.readFile(`blogPost/${file}`, 'utf-8')
+    const jsonParse = JSON.parse(fileData)
+    blogs.push(jsonParse)
+  }
+  res.status(200).json( blogs );
+  
+  
+  
+  
+  // , 'utf8', (err, data) => {
+  //   if (err) {
+  //     return res.status(500).json({ err });
+  //   }
+  //   data.forEach(file => {
+  //     , (fileErr, fileData) => {
+  //       if (fileErr) {
+  //         return res.status(500).json({err});
+  //       }
+  //       const file = JSON.parse(fileData)
+  //       blogs.push(file);
+  //       console.log(blogs)
+  //     });
+  //   });
+  // });
 }
